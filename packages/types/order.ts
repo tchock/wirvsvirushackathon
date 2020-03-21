68aeb41578deef68d88f4d9940155274e0bf6ef9
Berkan <berkan.yavri@deliveryhero.com>
2020-03-21 16:49:41 +0100

    Introduce graphql types in typescript (#5)

---
+++ packages/types/order.ts
@@ -0,0 +1,53 @@
+export interface Node {
+  nodeId: string;
+}
+
+export interface List<T> {
+  nodes: T[]
+}
+
+export interface Order extends Node {
+  nodeId: string;
+  pickUpCode: string;
+  confirmedPickUpTime?: string;
+  requestedPickUpTime?: string;
+  store: Store;
+  customer: Customer;
+  orderStatus: OrderStatus;
+  bundles: BundleList;
+  shareLink: string;
+}
+
+export interface Store extends Node {}
+export interface Customer extends Node {}
+export interface Bundle extends Node {
+  items: ItemList;
+}
+
+export interface Item extends Node {
+  quantity: number;
+  price: number;
+  name: string;
+  unit: string;
+}
+
+export enum OrderStatus {
+  ACCEPTED = 'ACCEPTED',
+  PICKED_UP = 'PICKED_UP',
+  REJECTED = 'REJECTED',
+}
+
+export enum Audiences {
+  CUSTOMER = 'CUSTOMER',
+  STORE = 'STORE',
+}
+
+export interface OrderInput {
+  bundles: BundleList;
+  store: string;
+  requestedPickUpTime: string;
+}
+
+export type BundleList = List<Bundle>;
+export type ItemList = List<Item>;
+export type OrderList = List<Order>;
