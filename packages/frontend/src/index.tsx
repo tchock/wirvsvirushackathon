640ff7cdcc5b8488b6c5cb8e78809dad64b62785
Simon Jentsch <simon.jentsch@deliveryhero.com>
2020-03-21 15:31:26 +0100

    feat(frontend): Add BundleOverview

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BundleOverview } from "./containers/BundleOverview";
+import { IntlProvider } from "react-intl";
 
-const App = () => <div>Hello World!</div>;
+const App = () => (
+  <IntlProvider locale="en">
+    <BundleOverview />
+  </IntlProvider>
+);
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
