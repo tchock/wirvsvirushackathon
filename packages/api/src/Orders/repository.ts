40d1515f1980aaa721c8506d4416dfb8ee06665b
Berkan Yavri <berkan.yavri@deliveryhero.com>
2020-03-21 22:44:43 +0100

    Introduce saveOrder, getOrderByPickUpCode and updateOrder

--- packages/api/src/Orders/repository.ts
+++ packages/api/src/Orders/repository.ts
@@ -2,12 +2,26 @@ import * as AWS from 'aws-sdk';
 import { Order } from 'types/order';
 import config from '../../config';
 
+enum DocumentTypes {
+  ORDER = 'ORDER',
+}
+
+type Document<T> = {
+  PK: string;
+  SK: string;
+  GSI_PK: string;
+  GSI_SK: string;
+  GSI_PK_2: string;
+  ItemType: DocumentTypes,
+  payload: T,
+}
+
 const tableName = process.env.DB_TABLE_MAIN;
 
 const documentClient: AWS.DynamoDB.DocumentClient = new AWS.DynamoDB.DocumentClient({
   apiVersion: '2012-08-10',
   endpoint: config.DB_ENDPOINT,
-  region: 'eu-west-1',
+  region: 'us-east-1',
   convertEmptyValues: true,
 })
 
@@ -23,6 +37,67 @@ export async function getOrder(nodeId): Promise<Order> {
       SK,
     }
   }
-  const result = await documentClient.get(params).promise();
+  const result = await documentClient
+    .get(params)
+    .promise();
   return result.Item as Order;
+}
+
+export async function saveOrder(order: Order): Promise<Order> {
+  const orderId = (new Buffer(order.nodeId, 'base64')).toString('utf-8');
+  const userId = (new Buffer(order.customer.nodeId, 'base64')).toString('utf-8');
+  const storeId = (new Buffer(order.store.nodeId, 'base64')).toString('utf-8');
+
+  const params: AWS.DynamoDB.DocumentClient.PutItemInput = {
+    TableName: tableName,
+    Item: {
+      PK: storeId,
+      SK: orderId,
+      GSI_PK: userId,
+      GSI_SK: orderId,
+      GSI_PK_2: order.pickUpCode,
+      ItemType: DocumentTypes.ORDER,
+      payload: order,
+    } as Document<Order>
+  }
+
+  await documentClient.put(params).promise();
+  return order;
+}
+
+export async function getOrderByPickUpCode(pickUpCode) {
+  const params: AWS.DynamoDB.DocumentClient.QueryInput = {
+    TableName: tableName,
+    IndexName: 'index',
+    KeyConditionExpression: 'GSI_PK_2 = :pickUpCode',
+    ExpressionAttributeValues: {
+      ':pickUpCode': pickUpCode,
+    },
+  };
+
+  return await documentClient.query(params).promise();
+}
+
+export async function updateOrder(order: Order): Promise<Order> {
+  const orderId = (new Buffer(order.nodeId, 'base64')).toString('utf-8');
+  const userId = (new Buffer(order.customer.nodeId, 'base64')).toString('utf-8');
+  const storeId = (new Buffer(order.store.nodeId, 'base64')).toString('utf-8');
+
+  const params: AWS.DynamoDB.DocumentClient.PutItemInput = {
+    TableName: tableName,
+    Item: {
+      PK: storeId,
+      SK: orderId,
+      GSI_PK: userId,
+      GSI_SK: orderId,
+      GSI_PK_2: order.pickUpCode,
+      ItemType: DocumentTypes.ORDER,
+      payload: order,
+    } as Document<Order>
+  };
+
+  await documentClient.put(params).promise();
+
+  // normally I would do getOrder but I think it's fine here
+  return order;
 }
