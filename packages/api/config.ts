c4980e07d39b7ef17076c21cfff83881fd92181c
Berkan <berkan.yavri@deliveryhero.com>
2020-03-22 00:08:50 +0100

    Introduce saveOrder, getOrderByPickUpCode and updateOrder (#20)

--- packages/api/config.ts
+++ packages/api/config.ts
@@ -1,4 +1,6 @@
 export default {
   DB_TABLE_MAIN: process.env.DB_TABLE_MAIN,
   DB_ENDPOINT: process.env.DB_ENDPOINT,
+  GSI_INDEX_NAME: process.env.GSI_INDEX_NAME,
+  GSI_2_INDEX_NAME: process.env.GSI_2_INDEX_NAME,
 }
--- packages/api/src/Orders/repository.ts
+++ packages/api/src/Orders/repository.ts
@@ -1,28 +1,163 @@
 import * as AWS from 'aws-sdk';
-import { Order } from 'types/order';
+import { Order, Audiences } from 'types/order';
 import config from '../../config';
+
+enum DocumentTypes {
+  ORDER = 'ORDER',
+}
+
+type Document<T> = {
+  PK: string;
+  SK: string;
+  GSI_PK: string;
+  GSI_SK: string;
+  GSI_PK_2: string;
+  ItemType: DocumentTypes,
+  payload: T,
+}
 
 const tableName = process.env.DB_TABLE_MAIN;
 
 const documentClient: AWS.DynamoDB.DocumentClient = new AWS.DynamoDB.DocumentClient({
   apiVersion: '2012-08-10',
   endpoint: config.DB_ENDPOINT,
-  region: 'eu-west-1',
+  region: 'us-east-1',
   convertEmptyValues: true,
-})
+});
 
-export async function getOrder(nodeId): Promise<Order> {
-  const orderIdentifier = (new Buffer(nodeId, 'base64')).toString('utf-8');
+export async function upsertOrder(order: Order): Promise<Order> {
+  const orderId = order.nodeId;
+  const userId = order.customer.nodeId;
+  const storeId = order.store.nodeId;
 
-  const [PK, SK] = orderIdentifier.split('::');
+  const params: AWS.DynamoDB.DocumentClient.PutItemInput = {
+    TableName: tableName,
+    Item: {
+      PK: `storeId_${storeId}`,
+      SK: `orderId_${orderId}`,
+      GSI_PK: `userId_${userId}`,
+      GSI_SK: `orderId_${orderId}`,
+      GSI_PK_2: `pickUpCode_${order.pickUpCode}`,
+      ItemType: DocumentTypes.ORDER,
+      payload: order,
+    } as Document<Order>
+  }
 
+  await documentClient.put(params).promise();
+  return order;
+}
+
+export async function getOrder(
+  nodeId: string,
+  userId: string,
+  userType: Audiences
+): Promise<Order> {
+  switch(userType) {
+    case Audiences.CUSTOMER:
+      return getOrderForCustomer(nodeId, userId);
+    case Audiences.STORE:
+      return getOrderForStore(nodeId, userId);
+  }
+}
+
+export async function getOrders(
+  userId: string,
+  userType: Audiences
+): Promise<Order[]> {
+  switch(userType) {
+    case Audiences.CUSTOMER:
+      return getOrdersForCustomer(userId);
+    case Audiences.STORE:
+      return getOrdersForStore(userId);
+  }
+}
+
+export async function getOrderByPickUpCode(pickUpCode) {
+  const params: AWS.DynamoDB.DocumentClient.QueryInput = {
+    TableName: tableName,
+    IndexName: config.GSI_2_INDEX_NAME,
+    KeyConditionExpression: 'GSI_PK_2 = :pickUpCode',
+    ExpressionAttributeValues: {
+      ':pickUpCode': `pickUpCode_${pickUpCode}`,
+    },
+  };
+
+  return await documentClient.query(params).promise();
+}
+
+async function getOrderForStore(userId: string, nodeId: string) {
   const params: AWS.DynamoDB.DocumentClient.GetItemInput = {
     TableName: tableName,
     Key: {
-      PK,
-      SK,
+      PK: userId,
+      SK: nodeId,
     }
   }
-  const result = await documentClient.get(params).promise();
+  const result = await documentClient
+    .get(params)
+    .promise();
   return result.Item as Order;
+}
+
+async function getOrderForCustomer(
+  nodeId: string,
+  userId: string,
+) {
+  const orders = await queryOrders<Order>(
+    'GSI_PK = :userId and GSI_SK = :nodeId',
+    {
+      ':userId': `userId_${userId}`,
+      ':nodeId': `orderId_${nodeId}`,
+    },
+    config.GSI_INDEX_NAME,
+  );
+
+  return orders[0].payload;
+}
+
+async function getOrdersForCustomer(userId: string) {
+  return (await queryOrders<Order>(
+    'GSI_PK = :userId',
+    {
+      ':userId': `userId_${userId}`,
+    },
+    config.GSI_INDEX_NAME,
+  )).map(orderDocument => orderDocument.payload);
+}
+
+async function getOrdersForStore(userId: string) {
+  return (await queryOrders<Order>(
+    'PK = :userId',
+    {
+      ':userId': `userId_${userId}`,
+    },
+  )).map(orderDocument => orderDocument.payload);
+}
+
+async function queryOrders<T>(
+  keyCondition: string,
+  expression: Record<string, string|number>,
+  index?: string,
+) {
+  const params: AWS.DynamoDB.DocumentClient.QueryInput = {
+    TableName: tableName,
+    KeyConditionExpression: keyCondition,
+    ExpressionAttributeValues: expression
+  }
+
+  if (index) {
+    params.IndexName = index;
+  }
+
+  const result = await documentClient
+    .query(params)
+    .promise();
+
+  const items = result.Items
+
+  if (items.length === 0) {
+    throw new Error('Record not found');
+  }
+
+  return items as Document<T>[];
 }
